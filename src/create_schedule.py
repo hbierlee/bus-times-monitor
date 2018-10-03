8fd3e2a60a9a11390fce5e22405baa7aa7ee8000
Henk Bierlee <bierlee.henk@gmail.com>
2018-10-03 23:42:37 +0200

    Replace route / trip specific join functions for general join function

--- src/create_schedule.py
+++ src/create_schedule.py
@@ -6,9 +6,9 @@ import operator
 UPPSALA_POLACKSBACKEN_STOP_ID = "740012548"
 
 DATA_PATH = os.path.join("../data/")
-STOP_TIMES_PATH = os.path.join(DATA_PATH, "original/stop_times.txt")
-TRIPS_PATH = os.path.join(DATA_PATH, "original/trips.txt")
-ROUTES_PATH = os.path.join(DATA_PATH, "original/routes.txt")
+STOP_TIMES_PATH = os.path.join(DATA_PATH, "stop_times.txt")
+TRIPS_PATH = os.path.join(DATA_PATH, "trips.txt")
+ROUTES_PATH = os.path.join(DATA_PATH, "routes.txt")
 
 SCHEDULE_DESTINATION_PATH = os.path.join(DATA_PATH, "schedules/schedule-" + str(int(time.time())) + ".csv")
 SCHEDULE_FIELDNAMES = ["trip_id", "route_id", "route_short_name", "trip_headsign", "departure_time"]
@@ -19,50 +19,39 @@ def write_csv_file(destination, fieldnames, rows):
 		writer.writeheader()
 		writer.writerows(rows)
 
+
 def sub_dict(somedict, somekeys, default=None):
 	return dict([ (k, somedict.get(k, default)) for k in somekeys ])
+
 
 def get_stop_times_for_stop_id(stop_id, stop_times_path=STOP_TIMES_PATH):
 	with open(stop_times_path, 'rb') as csvfile:
 		stop_times = csv.DictReader(csvfile, delimiter=',')
 		return filter(lambda row: row['stop_id'] == stop_id, stop_times)
 
-def append_trip_fields_to_schedule(schedule, trips_path=TRIPS_PATH):
-	with open(trips_path) as trips_file:
-		trips = csv.DictReader(trips_file)
-		sorting_key = operator.itemgetter("trip_id")
-		schedule = sorted(schedule, key=sorting_key)  # should already be the case
-		trips = sorted(trips, key=sorting_key)
 
-		# TODO can be improved by just using zip from the index of the first trip_id match (I think?)
-		tripIndex = 0;
-		for bus in schedule:
-			tripEntry = next({"trip": trip, "index": index} for index, trip in enumerate(trips) if trip["trip_id"] == bus["trip_id"])
-			tripIndex = tripEntry["index"]
-			bus.update(tripEntry["trip"])
-		return schedule
-
-
-def append_route_fields_to_schedule(schedule, routes_path=ROUTES_PATH):
-	with open(routes_path) as routes_file:
-		routes = csv.DictReader(routes_file)
-		sorting_key = operator.itemgetter("route_id")
+def inner_join_file_to_schedule(schedule, path, key):
+	with open(path) as file:
+		other_data = csv.DictReader(file)
+		
+		sorting_key = operator.itemgetter(key)
 		schedule = sorted(schedule, key=sorting_key)
-		routes = sorted(routes, key=sorting_key)  # should already be the case
+		other_data = sorted(other_data, key=sorting_key)  # should already be the case
 		
 		# TODO can be improved by just using zip from the index of the first trip_id match (I think?)
-		routeIndex = 0;
+		otherIndex = 0;
 		for bus in schedule:
-			routeEntry = next({"route": route, "index": index} for index, route in enumerate(routes) if route["route_id"] == bus["route_id"])
-			routeIndex = routeEntry["index"]
-			bus.update(routeEntry["route"])
+			routeEntry = next({"other": other, "index": index} for index, other in enumerate(other_data) if other[key] == bus[key])
+			otherIndex = routeEntry["index"]
+			bus.update(routeEntry["other"])
 		return schedule
 
 
 def create_schedule(stop_id):
 	schedule = get_stop_times_for_stop_id(stop_id)
-	append_trip_fields_to_schedule(schedule)
-	append_route_fields_to_schedule(schedule)
+	inner_join_file_to_schedule(schedule, TRIPS_PATH, "trip_id")
+	inner_join_file_to_schedule(schedule, ROUTES_PATH, "route_id")
+
 	schedule = map(lambda bus: sub_dict(bus, SCHEDULE_FIELDNAMES), schedule)
 
 	# TODO sort by times/route_id??
