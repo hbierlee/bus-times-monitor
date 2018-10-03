44dfd4ac920bf0597318332d7ef1f151c3bfea29
Henk Bierlee <bierlee.henk@gmail.com>
2018-10-03 22:25:12 +0200

    Improve the joining algorithm and add function that appends route fields to schedule

--- src/create_schedule.py
+++ src/create_schedule.py
@@ -1,12 +1,14 @@
 import os.path
 import csv
 import time
+import operator 
 
 UPPSALA_POLACKSBACKEN_STOP_ID = "740012548"
 
 DATA_PATH = os.path.join("../data/")
 STOP_TIMES_PATH = os.path.join(DATA_PATH, "original/stop_times.txt")
 TRIPS_PATH = os.path.join(DATA_PATH, "original/trips.txt")
+ROUTES_PATH = os.path.join(DATA_PATH, "original/routes.txt")
 
 SCHEDULE_DESTINATION_PATH = os.path.join(DATA_PATH, "schedules/schedule-" + str(time.time()) + ".txt")
 SCHEDULE_FIELDNAMES = ["trip_id","arrival_time","departure_time","stop_id","stop_sequence","pickup_type","drop_off_type", "route_id", "trip_headsign"]
@@ -20,24 +22,43 @@ def write_csv_file(destination, fieldnames, rows):
 def sub_dict(somedict, somekeys, default=None):
 	return dict([ (k, somedict.get(k, default)) for k in somekeys ])
 
-def get_stop_times_for_stop_id(stop_id):
+def get_stop_times_for_stop_id(stop_id, stop_times_path=STOP_TIMES_PATH):
 	print('get_stop_times_for_stop_id=' + UPPSALA_POLACKSBACKEN_STOP_ID)
-	with open(STOP_TIMES_PATH, 'rb') as csvfile:
+	with open(stop_times_path, 'rb') as csvfile:
 		stop_times = csv.DictReader(csvfile, delimiter=',')
 		return filter(lambda row: row['stop_id'] == stop_id, stop_times)
 
-def append_trip_fields_to_schedule(schedule, trips_path):
+def append_trip_fields_to_schedule(schedule, trips_path=TRIPS_PATH):
 	with open(trips_path) as trips_file:
 		trips = csv.DictReader(trips_file)
+		sorting_key = operator.itemgetter("trip_id")
+		schedule = sorted(schedule, key=sorting_key)  # should already be the case
+		trips = sorted(trips, key=sorting_key)
+		for bus, trip in zip(schedule, trips):
+			bus.update(trip)
+		return schedule
+		
 
-		for bus in schedule:
-			trip = next(trip for trip in trips if trip["trip_id"] == bus["trip_id"])
-			bus.update(sub_dict(trip, ["route_id", "trip_headsign"]))
+def append_route_fields_to_schedule(schedule, routes_path=ROUTES_PATH):
+	with open(routes_path) as routes_file:
+		routes = csv.DictReader(routes_file)
+		sorting_key = operator.itemgetter("route_id")
+		schedule = sorted(schedule, key=sorting_key)
+		routes = sorted(routes, key=sorting_key)  # should already be the case
+		for bus, route in zip(schedule, routes):
+			bus.update(route)
+		return schedule
+
 
 def create_schedule(stop_id):
+	print('get_stop_times_for_stop_id')
 	schedule = get_stop_times_for_stop_id(stop_id)
-	append_trip_fields_to_schedule(schedule, TRIPS_PATH)
-	write_csv_file(SCHEDULE_DESTINATION_PATH, SCHEDULE_FIELDNAMES, schedule)
+	append_trip_fields_to_schedule(schedule)
+	print("append_trip_fields_to_schedule")
+	append_route_fields_to_schedule(schedule)
+	print("append_route_fields_to_schedule")
+	print(schedule)
+	write_csv_file(SCHEDULE_DESTINATION_PATH, schedule[0].keys(), schedule)
 
 create_schedule(UPPSALA_POLACKSBACKEN_STOP_ID)
 
