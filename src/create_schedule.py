633c96eb5b98bae4665081774ab3d6c3c9f076d8
Henk Bierlee <bierlee.henk@gmail.com>
2018-10-03 21:58:37 +0200

    Add function to append route info to stop_times

--- src/create_schedule.py
+++ src/create_schedule.py
@@ -1,35 +1,45 @@
 import os.path
 import csv
 import time
-print(time.time())
-print(type(time.time()))
 
 UPPSALA_POLACKSBACKEN_STOP_ID = "740012548"
 
 DATA_PATH = os.path.join("../data/")
-STOP_TIMES_PATH = os.path.join(DATA_PATH, "original/stop_times_small.txt")
+STOP_TIMES_PATH = os.path.join(DATA_PATH, "original/stop_times.txt")
+TRIPS_PATH = os.path.join(DATA_PATH, "original/trips.txt")
+
 SCHEDULE_DESTINATION_PATH = os.path.join(DATA_PATH, "schedules/schedule-" + str(time.time()) + ".txt")
+SCHEDULE_FIELDNAMES = ["trip_id","arrival_time","departure_time","stop_id","stop_sequence","pickup_type","drop_off_type", "route_id", "trip_headsign"]
 
 def write_csv_file(destination, fieldnames, rows):
-	print('start writing ')
 	with open(destination, 'w') as file:
 		writer = csv.DictWriter(file, fieldnames=fieldnames)
 		writer.writeheader()
 		writer.writerows(rows)
 
-def find_all_stop_times_for_stop_id(stop_id):
+def sub_dict(somedict, somekeys, default=None):
+	return dict([ (k, somedict.get(k, default)) for k in somekeys ])
+
+def get_stop_times_for_stop_id(stop_id):
+	print('get_stop_times_for_stop_id=' + UPPSALA_POLACKSBACKEN_STOP_ID)
 	with open(STOP_TIMES_PATH, 'rb') as csvfile:
 		stop_times = csv.DictReader(csvfile, delimiter=',')
-		print('filtering stop_times on stop_id=' + UPPSALA_POLACKSBACKEN_STOP_ID)
-		
-		filtered_stop_times = filter(lambda row: row['stop_id'] == stop_id, stop_times)
+		return filter(lambda row: row['stop_id'] == stop_id, stop_times)
 
-		print(filtered_stop_times)
+def append_trip_fields_to_schedule(schedule, trips_path):
+	with open(trips_path) as trips_file:
+		trips = csv.DictReader(trips_file)
 
-		write_csv_file(SCHEDULE_DESTINATION_PATH, stop_times.fieldnames, filtered_stop_times)
+		for bus in schedule:
+			trip = next(trip for trip in trips if trip["trip_id"] == bus["trip_id"])
+			bus.update(sub_dict(trip, ["route_id", "trip_headsign"]))
 
+def create_schedule(stop_id):
+	schedule = get_stop_times_for_stop_id(stop_id)
+	append_trip_fields_to_schedule(schedule, TRIPS_PATH)
+	write_csv_file(SCHEDULE_DESTINATION_PATH, SCHEDULE_FIELDNAMES, schedule)
 
-find_all_stop_times_for_stop_id(UPPSALA_POLACKSBACKEN_STOP_ID)
+create_schedule(UPPSALA_POLACKSBACKEN_STOP_ID)
 
 
 
