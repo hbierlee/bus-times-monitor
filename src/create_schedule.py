ad261a5edb09458ab0f4789f5201aefa00ee6cf8
Henk Bierlee <bierlee.henk@gmail.com>
2018-10-04 00:01:16 +0200

    Join calendar and calendar_date fields to schedule

--- src/create_schedule.py
+++ src/create_schedule.py
@@ -9,9 +9,11 @@ DATA_PATH = os.path.join("../data/")
 STOP_TIMES_PATH = os.path.join(DATA_PATH, "stop_times.txt")
 TRIPS_PATH = os.path.join(DATA_PATH, "trips.txt")
 ROUTES_PATH = os.path.join(DATA_PATH, "routes.txt")
+CALENDAR_PATH = os.path.join(DATA_PATH, "calendar.txt")
+CALENDAR_DATES_PATH = os.path.join(DATA_PATH, "calendar_dates.txt")
 
 SCHEDULE_DESTINATION_PATH = os.path.join(DATA_PATH, "schedules/schedule-" + str(int(time.time())) + ".csv")
-SCHEDULE_FIELDNAMES = ["trip_id", "route_id", "route_short_name", "trip_headsign", "departure_time"]
+SCHEDULE_FIELDNAMES = ["trip_id", "route_id", "service_id", "route_short_name", "trip_headsign", "departure_time", "monday","tuesday","wednesday","thursday","friday","saturday","sunday","start_date", "end_date", "date", "exception_type"]
 
 def write_csv_file(destination, fieldnames, rows):
 	with open(destination, 'w') as file:
@@ -36,21 +38,24 @@ def inner_join_file_to_schedule(schedule, path, key):
 		
 		sorting_key = operator.itemgetter(key)
 		schedule = sorted(schedule, key=sorting_key)
-		other_data = sorted(other_data, key=sorting_key)  # should already be the case
+		other_data = sorted(other_data, key=sorting_key)
 		
 		# TODO can be improved by just using zip from the index of the first trip_id match (I think?)
 		otherIndex = 0;
 		for bus in schedule:
-			routeEntry = next({"other": other, "index": index} for index, other in enumerate(other_data) if other[key] == bus[key])
+			routeEntry = next({"other": other, "index": index} for index, other in enumerate(other_data[otherIndex+1:]) if other[key] == bus[key])
 			otherIndex = routeEntry["index"]
 			bus.update(routeEntry["other"])
 		return schedule
 
 
 def create_schedule(stop_id):
+	print("create_schedule")
 	schedule = get_stop_times_for_stop_id(stop_id)
 	inner_join_file_to_schedule(schedule, TRIPS_PATH, "trip_id")
 	inner_join_file_to_schedule(schedule, ROUTES_PATH, "route_id")
+	inner_join_file_to_schedule(schedule, CALENDAR_PATH, "service_id")
+	inner_join_file_to_schedule(schedule, CALENDAR_DATES_PATH, "service_id")
 
 	schedule = map(lambda bus: sub_dict(bus, SCHEDULE_FIELDNAMES), schedule)
 
