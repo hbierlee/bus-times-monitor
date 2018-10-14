446037fb51a6633df524308ee686720d83ea69c8
Ahmed <30323515+AMHD@users.noreply.github.com>
2018-10-14 16:45:16 +0300

    display.py
    Modified version of the code, that displays the time on each of the four quadrants.

--- src/display.py
+++ src/display.py
@@ -1,16 +1,16 @@
-# from sense_hat import SenseHat
-# sense = SenseHat()
+from sense_hat import SenseHat
+sense = SenseHat()
 
 
-def assigner(time, color_a, color_b, lights):
+def display_quadrant(time, color_a, color_b, lights):
     size = len(lights) - 1
     for i in range(len(lights)):
-        if i == 0:
-            lights[size] = color_a
-        elif i >= time:
-            lights[size - i] = color_b
-        else:
+        if time < 16 and i == 0:
+            lights[size] = color_b
+        elif time >= (size - i) + 1:
             lights[size - i] = color_a
+        else:
+            lights[size - i] = color_b
     return lights
 
 
@@ -19,7 +19,7 @@ def busA(time):
     green = [0, 128, 0]
     white = [255, 255, 255]
     busALights = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
-    return assigner(time, green, white, busALights)
+    return display_quadrant(time, green, white, busALights)
 
 
 def busB(time):
@@ -27,7 +27,7 @@ def busB(time):
     blue = [0, 0, 255]
     white = [255, 255, 255]
     busBLights = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
-    return assigner(time, blue, white, busBLights)
+    return display_quadrant(time, blue, white, busBLights)
 
 
 def busC(time):
@@ -35,7 +35,7 @@ def busC(time):
     yellow = [255, 255, 0]
     white = [255, 255, 255]
     busCLights = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
-    return assigner(time, yellow, white, busCLights)
+    return display_quadrant(time, yellow, white, busCLights)
 
 
 def busD(time):
@@ -43,17 +43,54 @@ def busD(time):
     red = [255, 0, 0]
     white = [255, 255, 255]
     busDLights = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
-    return assigner(time, red, white, busDLights)
+    return display_quadrant(time, red, white, busDLights)
 
 
-def combine(a, b, c, d):
-    return [busA(a), busB(b), busC(c), busD(d)]
+def display_first(a):
+    counter = 0
+    for i in range(0, 4):
+        for j in range(0, 4):
+            print(str(i) + "\t" + str(j))
+            #print(a[counter])
+            sense.set_pixel(i, j, a[counter])
+            counter += 1
+
+
+def display_second(a):
+    counter = 0
+    for i in range(4, 8):
+        for j in range(0, 4):
+            #print(a[counter])
+            #print(str(i) + "\t" + str(j))
+            sense.set_pixel(i, j, a[counter])
+            counter += 1
+
+
+def display_third(a):
+    counter = 0
+    for i in range(0, 4):
+        for j in range(4, 8):
+            #print(a[counter])
+            #print(str(i) + "\t" + str(j))
+            sense.set_pixel(i, j, a[counter])
+            counter += 1
+
+
+def display_fourth(a):
+    counter = 0
+    for i in range(4, 8):
+        for j in range(4, 8):
+            #print(a[counter])
+            #print(str(i) + "\t" + str(j))
+            sense.set_pixel(i, j, a[counter])
+            counter += 1
 
 
 def main():
-    #sense.set_pixels(combine(80, 5, 15, 55))
-    print combine(80, 5, 15, 55)
-
+    display_first(busA(5))
+    display_second(busB(5))
+    display_third(busC(8))
+    display_fourth(busD(11))
 
 if __name__ == '__main__':
     main()
